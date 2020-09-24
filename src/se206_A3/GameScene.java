a6c61f0aead82d81cf13ed72fd6c3bbf78f1dc64
JiaQi <jli280@aucklanduni.ac.nz>
2020-09-24 17:59:27 +1200

    added update; removes questions answered

--- src/se206_A3/GameScene.java
+++ src/se206_A3/GameScene.java
@@ -3,6 +3,7 @@ package se206_A3;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -250,8 +251,9 @@ public class GameScene {
 					ttsA.start();
 					a.setHeaderText("The correct answer was " + answer);
 				}
+				update(category, lineNum);
 				a.showAndWait();
-				_primary.setScene(_game);
+				startScene();
 			}	
 		});
 		
@@ -264,5 +266,42 @@ public class GameScene {
 		
 		layout.getChildren().addAll(direction, txtInput, btnEnter, dkBtn);
 		return layout;
+	}
+	
+	/*
+	 * Method updates save files so that questions which have been answered are removed
+	 */
+	public void update(String cateFile, int lineRemove) {
+		lineRemove++;
+		File inputFile = new File("./saves/"+cateFile);
+		File tmp = new File("./saves/"+cateFile+"Tmp");
+		
+		BufferedReader reader = null;
+		BufferedWriter writer = null;
+		try {
+			reader = new BufferedReader(new FileReader(inputFile));	
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String currentLine;
+		int count = 0;
+		try {
+			writer = new BufferedWriter(new FileWriter(tmp));
+			while((currentLine = reader.readLine()) != null) {
+				count++;
+				if (count == lineRemove) {
+					continue;
+				}
+				writer.write(currentLine + System.getProperty("line.separator"));
+			}
+			writer.close();
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		inputFile.delete();
+		tmp.renameTo(inputFile);
 	}
 }
