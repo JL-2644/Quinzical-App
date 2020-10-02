29a506b50d7ada7587103884fcd14eb8790db242
yli667 <69701294+yli667@users.noreply.github.com>
2020-10-02 16:53:45 +1300

    added comments

--- src/se206_A3/PracticeScene.java
+++ src/se206_A3/PracticeScene.java
@@ -38,15 +38,19 @@ public class PracticeScene {
 	private int _attempts = 0;
 	Button _back = new Button("Main Menu");
 
+	//Constructor
 	public PracticeScene(String[] catNames, Stage primary, Scene menu) {
 		_primary = primary;
 		_menu = menu;
 		_catNames = catNames;
 	}
 
+	//Method to start the scene
 	public void startScene() {
+		//Initialize the data(questions, answers, categories)
 		initial();
 		int count = 0;
+		//button for going to main menu
 		_back.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
@@ -63,6 +67,7 @@ public class PracticeScene {
 		vbox.getChildren().add(label);
 		VBox.setMargin(label, new Insets(10, 10, 10, 10));
 		VBox.setMargin(_back, new Insets(10, 10, 10, 10));
+		//create buttons according to amount of categories
 		for (String cat:_cat) {
 			Button catButton = new Button(cat);
 			vbox.getChildren().add(catButton);
@@ -70,17 +75,22 @@ public class PracticeScene {
 			catList.add(catButton);
 			count++;
 		}
+		//Practice scene
 		Scene scene = new Scene(vbox, 500, 45 * count + 124);
 		_primary.setTitle("Practice");
 		_primary.setScene(scene);
 
+		//Set up for every category buttons
 		for (Button cat:catList) {
 			cat.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle (ActionEvent e) {
 					int tmp = catList.indexOf(cat);
+					
+					//Find a random question
 					int ran = getRandomElement(question.get(tmp));
 					String randomQuestion = question.get(tmp).get(ran);
+					
 					Label label2 = new Label("Question");
 					label2.setFont(new Font("Arial", 24));
 					VBox vbox2 = new VBox(5);
@@ -91,6 +101,8 @@ public class PracticeScene {
 					TextField answerTxt = new TextField();
 					answerTxt.setMaxWidth(180);
 					Button confirm = new Button("Submit");
+					
+					//set up slider for tts
 					Slider slider = new Slider();
 					slider.setMin(0.5);
 					slider.setMax(2);
@@ -99,11 +111,15 @@ public class PracticeScene {
 					slider.setShowTickMarks(true);
 					slider.setBlockIncrement(0.25);
 					slider.setMaxWidth(180);
+					
 					VBox.setMargin(que, new Insets(10, 10, 10, 10));
 					VBox.setMargin(slider, new Insets(10, 10, 10, 10));
+					
 					Label clueLabel = new Label(clue.get(tmp).get(ran) + ": ...");
 					Label info = new Label("Adjust question speed");
 					VBox.setMargin(clueLabel, new Insets(10, 10, 10, 10));
+					
+					//replay button
 					Button replay = new Button("Replay");
 					replay.setOnAction(new EventHandler<ActionEvent>() {
 						@Override
@@ -118,9 +134,11 @@ public class PracticeScene {
 					VBox.setMargin(confirm, new Insets(10, 10, 10, 10));
 					vbox2.getChildren().addAll(label2, que, slider, info, replay, answerTxt, confirm);
 					Scene scene2 = new Scene(vbox2, (int) len + 100, 450);
+					//tts the question
 					HelperThread ttsQ = new HelperThread(randomQuestion);
 					ttsQ.start();
 
+					//change scene to the random question
 					_primary.setScene(scene2);
 					_primary.centerOnScreen();
 					_attempts = 0;
@@ -128,23 +146,24 @@ public class PracticeScene {
 					confirm.setOnAction(new EventHandler<ActionEvent>() {
 						@Override
 						public void handle(ActionEvent e) {
-							// Get user input, check if it matches the actual answer,
-							// if yes winning would increase, otherwise it would decrease.
-							// Change back to main menu scene after answering the question.
-							// Attempted questions' information would be deleted from the lists.
+							// Get user input, check if it matches the actual answer
 							String sen;
 							String userAns = answerTxt.getText().trim().toLowerCase();
 							String fullAns = answer.get(tmp).get(ran);
 							boolean status = false;
+							//If answer contains "/" then check all possible answers
 							if (fullAns.contains("/") == true) {
 								String[] parts = fullAns.split("/");
 								for (String diffAns : parts) {
+									//if it matches, status would be true and it would break the for loop
 									if (userAns.equals(diffAns.trim().toLowerCase())) {
 										status = true;
 										break;
 									}
 								}
 							}
+							
+							//if user answer equals actual answer or if status is true
 							if (userAns.equals(fullAns.toLowerCase()) || status == true) {
 
 								sen = "Correct!!!";
@@ -157,6 +176,7 @@ public class PracticeScene {
 								_primary.centerOnScreen();
 							} else {
 								_attempts++;
+								//After all attempts
 								if (_attempts == 3) {
 									Label label3 = new Label("Question");
 									label3.setFont(new Font("Arial", 24));
@@ -167,6 +187,8 @@ public class PracticeScene {
 									VBox.setMargin(label3, new Insets(10, 10, 10, 10));
 									VBox.setMargin(label4, new Insets(10, 10, 10, 10));
 									Text que = new Text(randomQuestion);
+									
+									//Button to go back to practice module
 									Button practice = new Button("Practice Module");
 									practice.setOnAction(new EventHandler<ActionEvent>() {
 										@Override
@@ -176,11 +198,14 @@ public class PracticeScene {
 											_primary.centerOnScreen();
 										}
 									});
+									//show user the correct answer
 									sen = "The correct answer is: " + answer.get(tmp).get(ran);
 									Text ans = new Text(sen);
 									VBox.setMargin(que, new Insets(10, 10, 10, 10));
 									VBox.setMargin(practice, new Insets(10, 10, 10, 10));
 									VBox.setMargin(ans, new Insets(10, 10, 10, 10));
+									
+									//tts the answer
 									HelperThread ttsQ = new HelperThread(sen);
 									ttsQ.start();
 									vbox3.getChildren().addAll(label3, que, label4, ans, _back, practice);
@@ -188,6 +213,8 @@ public class PracticeScene {
 									_primary.setScene(scene3);
 									_primary.centerOnScreen();
 								} else {
+									//not revealing the correct answer if the user hasn't answered the questions threee times
+									//already
 									sen = "Wrong Answer";
 									HelperThread ttsQ = new HelperThread(sen);
 									ttsQ.start();
@@ -195,12 +222,15 @@ public class PracticeScene {
 									a.setTitle("Result");
 									a.showAndWait();
 
+									//User gets the first letter of the answer as a hint
 									if (_attempts == 2) {
 										Text hint = new Text("Hint: " + answer.get(tmp).get(ran).charAt(0));
 										VBox vbox4 = new VBox(5);
 										vbox4.setAlignment(Pos.CENTER);
 										vbox4.getChildren().addAll(label2, que, slider, info, replay, hint, clueLabel,
 												answerTxt, confirm);
+										
+										//change to a scene that contain hint
 										Scene scene4 = new Scene(vbox4, (int) len + 100, 450);
 										_primary.setScene(scene4);
 										_primary.centerOnScreen();
@@ -251,7 +281,7 @@ public class PracticeScene {
 		}
 	}
 
-	//Read file to give useful information such as values, questions and answers.
+	//Read file to give useful information such as clues, questions and answers.
 	public void readFile(String file) {
 		String line = "";
 		String split = "\\|";
@@ -279,6 +309,7 @@ public class PracticeScene {
 		}
 	}
 
+	//get random position from a list
 	public int getRandomElement(List<String> list) {
 		Random rand = new Random();
 		return rand.nextInt(list.size());
