37eb470d9d781907b7e6879d6acf02b23260723e
yli667 <69701294+yli667@users.noreply.github.com>
2020-10-16 17:00:21 +1300

    Refactored code. Used by Practice scene

---
+++ src/se206_A3/InitialData.java
@@ -0,0 +1,60 @@
+package se206_A3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InitialData {
+	
+	public InitialData () {
+	}
+
+	/*
+	 * Initialize the information used for the game from categories/ folder.
+	 */
+	public void initial(String[] catNames, List<String> cat, List<List<String>> question, 
+			List<List<String>> clue, List<List<String>> answer) {
+				
+		try {
+				for (String line:catNames) {
+					cat.add(line);
+					String file = "categories/" + line;
+					readFile(file, question, clue, answer);
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Read file to give useful information such as clues, questions and answers.
+	 */
+	public void readFile(String file, List<List<String>> question, 
+			List<List<String>> clue, List<List<String>> answer) {
+		String line = "";
+		String split = "\\|";
+		List<String> questionTmp = new ArrayList<String>();
+		List<String> answerTmp = new ArrayList<String>();
+		List<String> clueTmp = new ArrayList<String>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+			while ((line = br.readLine()) != null) {
+
+				String[] after = line.split(split);
+
+				questionTmp.add(after[0]);
+				clueTmp.add(after[1]);
+				answerTmp.add(after[2].trim());
+			}
+
+			question.add(questionTmp);
+			clue.add(clueTmp);
+			answer.add(answerTmp);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
